83eb034ab0fd571e2cd370bc5032a93d92f591cb
Nervi <jhonfernando71284489@gmail.com>
2024-02-29 05:08:38 -0400

    prepado

---
+++ soap_server.py
@@ -0,0 +1,26 @@
+from http.server import HTTPServer
+from pysimplesoap.server import SoapDispatcher, SOAPHandler
+
+def SumaDosNumeros(a,b):
+    return "la suma de ambos es: ",a+b
+
+dispatcher = SoapDispatcher(
+    "soap_server",
+    location="http://localhost:8000/",
+    action="http://localhost:8000/",
+    namespace="http://localhost:8000/",
+    trace=True,
+    ns=True,
+)
+
+dispatcher.register_function(
+    "SumaDosNumeros",
+    SumaDosNumeros,
+    returns={"suma: ": str},
+    args={"suma": str},
+)
+
+server=HTTPServer(("0.0.0.0",8000),SoapDispatcher)
+server.dispatcher=dispatcher
+print("servidor soap iniciado en http://localhost:8000/")
+server.serve_forever()
